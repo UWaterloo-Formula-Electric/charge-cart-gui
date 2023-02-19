7f3b4ad915c1c2f87178698a20ade021235f92e7
tony3529353@gmail.com <tony3529353@gmail.com>
2023-02-19 12:31:18 -0500

    Add a table for 70 cells and multiple tabs

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6 import uic
-from output import Ui_MainWindow
-
-class UI(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        # loading the ui file with uic module
-        # uic.loadUi("untitled.ui", self)
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
+import sys
 
 
-app = QApplication([])
-window = Ui_MainWindow()
-window.show()
-app.exec()
+# python3 -m PyQt6.uic.pyuic -o testing.py -x untitled.ui
+# python3 -m PyQt6.uic.pyuic -o cart_testing.py -x untitled.ui
+
+class MyWindow(QMainWindow):
+    def  __init__(self):
+        super(MyWindow,self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Tony's Candy Shop")
+        self.initUI()
+
+    def initUI(self):
+        self.label1 = QtWidgets.QLabel(self)
+        # this class is a QMainWindow object, so
+        # instead of putting in an QMainWindow object, we just put self
+        self.label1.setText("my first label")
+        self.label1.move(50, 0)
+
+        b1 = QtWidgets.QPushButton(self)
+        b1.setText("click me")
+        b1.move(100, 100)
+        b1.clicked.connect(self.clicking_event)
+
+    def clicking_event(self):
+        self.label1.setText("you have pressed the button")
+        self.update()
+
+    def update(self):
+        self.label1.adjustSize()
+
+
+
+def button_clicked():
+    print()
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+
+    win.show()
+    sys.exit(app.exec())
+
+
+window()
