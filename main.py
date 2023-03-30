041f93204158f8697e45f1ecaa51036c96de57de
tony3529353@gmail.com <tony3529353@gmail.com>
2023-03-30 02:01:32 -0400

    added virtual data, updating voltage functions

--- main.py
+++ main.py
@@ -1,51 +1,135 @@
 from PyQt6 import QtWidgets, QtCore
 from PyQt6.QtWidgets import QApplication, QMainWindow
-# import serial_parse_test as data_parsing
+from PyQt6.QtCore import QObject, QThread, pyqtSignal
+import serial_parse_test
 from testing1 import Ui_MainWindow
 import sys
+from numpy import random
+from time import sleep
 
 
 # python3 -m PyQt6.uic.pyuic -o testing.py -x untitled.ui
 # python3 -m PyQt6.uic.pyuic -o cart_testing.py -x untitled.ui
+
+class Worker(QObject):
+    finished = pyqtSignal()
+    progress1 = pyqtSignal(int)
+
+    log = pyqtSignal(str)
+
+    # dict maybe?
+    batteryInfo = pyqtSignal(list)
+
+    progress2 = pyqtSignal(int)
+
+    def run(self):
+        # update 5 times
+        while True:
+            self.updateBatteryInfo()
+            sleep(5)
+            self.update_SoC()
+
+
+    def updateBatteryInfo(self):
+        # connect to the port
+        # emit a list of batteries
+        self.batteryInfo.emit([{"battery": 3}])
+
+        # connector = serial_parse_test.SerialConnect()
+        # if (connector.port_setup() == True):
+        #     connector.execute()
+        #     self.batteryInfo.emit(connector.get_battInfo())
+        #
+        # else:
+        #     self.log.emit("connection failed!")
+
+        self.finished.emit()
+
+
+    def update_SoC(self):
+        # for i in range(50):
+        #     sleep(0.5)
+
+        # trigger functions and pass in values
+        # whenever is passed in emit will be passed in whatever we connect with (update_SoC)
+        self.progress2.emit(50)
+        self.finished.emit()
+
 
 
 class MyWindow(Ui_MainWindow, QtWidgets.QWidget):
     def  __init__(self):
         super(MyWindow, self).__init__()
         self.setupUi(MainWindow)
+
         # force to show the main page first
         self.CellTab.setCurrentIndex(0)
+        self.SOC_progressBar.setValue(0)
 
         # charging state
-        self.state = "Pause"
+        self.state = "Charging"
+        self.progressVal = 0
 
-
-
-
-
+        # Make sure this is false
+        self.isConntecd = True
 
         # connect all the buttons
         self.setCurrent_pb.clicked.connect(self.adjustCurrent)
-        self.connect_pb.clicked.connect(self.connectPort)
-        self.startBalancing_pb.clicked.connect(self.startBalancing)
-        self.startCharging_pb.clicked.connect(self.Charging)
+
+        self.connect_pb.clicked.connect(self.updateData)
+
+        self.startBalancing_pb.clicked.connect(self.getMeanMaxMinofVoltage)
+        self.startCharging_pb.clicked.connect(self.charging)
+
+        # maybe I should add another button for start?
+        # self.connect_pb.clicked.connect(self.updateBatteryInfo())
+
+
+        self.BoxesList = []
+        self.BoxesList.append(self.box1_odd)
+        self.BoxesList.append(self.box1_even)
+        self.BoxesList.append(self.box2_odd)
+        self.BoxesList.append(self.box2_even)
+        self.BoxesList.append(self.box3_odd)
+        self.BoxesList.append(self.box3_even)
+        self.BoxesList.append(self.box4_odd)
+        self.BoxesList.append(self.box4_even)
+        self.BoxesList.append(self.box5_odd)
+        self.BoxesList.append(self.box5_even)
+
+    def updateData(self):
+        # Stop GUI from freezing as program runs
+        # https://realpython.com/python-pyqt-qthread
+
+        # ================================================
+        # Create QThread and Worker objects
+        self.thread = QThread()
+        # Create a worker object
+        self.worker = Worker()
+
+        # Move worker to the thread
+        self.worker.moveToThread(self.thread)
+
+        # Connect signals and slots
+        self.thread.started.connect(self.worker.run)
+
+        self.worker.finished.connect(self.thread.quit)
+
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+
+        # self.worker.progress1.connect(self.updateBatteryInfo)
+        self.worker.batteryInfo.connect(self.updateBatteryInfo)
+        self.worker.log.connect(self.updateLog)
+
+
+        self.worker.progress2.connect(self.update_SoC)
+
+        # Start thread
+        self.thread.start()
 
 
 
-        # self.timer = QtCore.QTimer()
-        # self.timer.setInterval(10)
-        # self.timer.timeout.connect(self.update)
-        # self.timer.start()
-        # self.update()
-        # self.setCurrent_input.repaint()
-
-
-    def paintEvent(self, event):
-        self.setCurrent_input.setText("This is working")
-        print(self.setCurrent_input.toPlainText())
-
-    def update(self):
-        self.repaint()
 
 
     # all the functions being called
@@ -54,23 +138,21 @@ class MyWindow(Ui_MainWindow, QtWidgets.QWidget):
 
     def connectPort(self):
         self.logging_texbox.appendPlainText("connecting to STLink...")
-
-
         # wait until debug serial connection
-        ''' 
-        message = data_parsing.port_setup()
-        if message == "Failed":
-            self.logging_texbox.appendPlainText(message)
-        else:
-            self.logging_texbox.appendPlainText(message)
-            self.logging_texbox.appendPlainText("STLink connected")
-            
-        '''
+        # message = data_parsing.port_setup()
+        # if message == "Failed":
+        #     self.logging_texbox.appendPlainText(message)
+        # else:
+        #     self.logging_texbox.appendPlainText(message)
+        #     self.logging_texbox.appendPlainText("STLink connected")
+        #     self.isConntecd = True
+
+
 
     def startBalancing(self):
         self.logging_texbox.appendPlainText("start balancing")
 
-    def Charging(self):
+    def charging(self):
         if self.state == "Pause":
             self.logging_texbox.appendPlainText("Charging...")
             self.state = "Charging"
@@ -81,46 +163,162 @@ class MyWindow(Ui_MainWindow, QtWidgets.QWidget):
         self.startCharging_pb.setText(self.state)
 
 
+    # Testing: only for updating
+    def virtualBatteryInfo_Splited(self):
+        virtual70Cell = []
+
+        for batch in range(0, 5):
+            virtualdict = {}
+            for cell in range(1,15):
+                virtualdict[f"cell_{14 * batch + cell}"] = {"voltage": random.randint(100) , "temp": random.randint(100)}
+
+            virtual70Cell.append(virtualdict)
+
+
+        print(virtual70Cell)
+        return virtual70Cell
+
+
+    def virtualBatteryInfo_UnSplited(self):
+        virtualdict = {}
+        for cell in range(0, 70):
+            virtualdict[f"cell_{cell}"] = {"voltage": random.randint(100) , "temp": random.randint(100)}
+
+        print(virtualdict)
+        return virtualdict
+
+    def getMeanMaxMinofVoltage(self, batteryInfo):
+        # Testing
+        virtual70Cells = self.virtualBatteryInfo_UnSplited()
+
+        totalVoltage = 0
+        maxVol = virtual70Cells[f"cell_1"]["voltage"]
+        minVol = virtual70Cells[f"cell_1"]["voltage"]
+        maxTemp = virtual70Cells[f"cell_1"]["temp"]
+        minTemp = virtual70Cells[f"cell_1"]["temp"]
+
+        for i in range(70):
+            singleVol = virtual70Cells[f"cell_{i}"]["voltage"]
+            singleTemp = virtual70Cells[f"cell_{i}"]["temp"]
+            totalVoltage += singleVol
+            if(singleVol > maxVol):
+                maxVol = singleVol
+            if (singleVol < minVol):
+                minVol = singleVol
+
+            if(singleTemp > maxTemp):
+                maxTemp = singleTemp
+            if (singleTemp < minTemp):
+                minTemp = singleTemp
 
 
 
 
-    # def update(self):
-    #     self.plainTextEdit.adjustSize()
+        mean = totalVoltage/70
+
+        self.maxVolt_textbox.setText(str(maxVol))
+        self.minVolt_textbox.setText(str(minVol))
+        self.maxTemp_textbox.setText(str(maxTemp))
+        self.minTemp_textbox.setText(str(minTemp))
 
 
 
 
 
-# def window():
-#     app = QApplication(sys.argv)
-#     win = MyWindow()
-#
-#     win.show()
-#     sys.exit(app.exec())
+
+    def updateBatteryInfo(self, batteryInfo):
+        # batteryInfo
+        '''
+        batteryInfo = [
+        {
+            "cell_1": {
+                "voltage": 36,
+                "temp": 25,
+            },
+
+            "cell_2": {
+                "voltage": 36,
+                "temp": 25,
+            }
+        },
+
+        {
+            "cell_3": {
+                "voltage": 3.6,
+                "temp": 25,
+            },
+
+            "cell_4": {
+                "voltage": 3.6,
+                "temp": 25,
+            }
+        }
+        ]
+
+        '''
 
 
-# virtual data for testing
-cell_data = {
-    "cell_0": {
-        "voltage": 3.6,
-        "temp": 25,
-    },
 
-    "cell_1": {
-        "voltage": 2.6,
-        "temp": 100,
-    },
 
-    "cell_2": {
-        "voltage": 1.3,
-        "temp": 75,
-    }
+        # self.logging_texbox.appendPlainText("updating battery Info")
 
-}
+        batch_Index = 0
+        Num_Cell_Per_Batch = 13
+        Num_Batch = 5
+        cellIndex = 1
+        virtual70Cells = self.virtualBatteryInfo_Splited()
+        print(virtual70Cells)
+
+        # iterate through 10 tables (5 pairs)
+        for BoxesIndex in range(0,Num_Batch*2,2):
+            curCellBatch = virtual70Cells[batch_Index]
+
+            self.log(curCellBatch)
+
+
+            lowerBound = cellIndex
+            rowIndex = 0
+
+            print("From", cellIndex)
+            print("To", lowerBound+Num_Cell_Per_Batch)
+            print("CurrentCell", cellIndex)
+
+            # iterate over 7 cells in a batch / two tables
+            for cellIndex in range(cellIndex, lowerBound+Num_Cell_Per_Batch, 2):
+                # dictionary
+                odd = str(curCellBatch[f"cell_{cellIndex}"]["voltage"])
+                even = str(curCellBatch[f"cell_{cellIndex+1}"]["voltage"])
+
+                print(cellIndex)
+                self.BoxesList[BoxesIndex].setItem(rowIndex, 0, QtWidgets.QTableWidgetItem(odd))
+                self.BoxesList[BoxesIndex+1].setItem(rowIndex, 0, QtWidgets.QTableWidgetItem(even))
+
+                rowIndex+=1
+
+            batch_Index+=1
+            cellIndex = lowerBound+Num_Cell_Per_Batch
+            cellIndex+=1
+
+
+    def update_SoC(self, n):
+        self.SOC_progressBar.setValue(n)
+        self.logging_texbox.appendPlainText("updating state of charge")
+
+    def updateLog(self, log):
+        self.logging_texbox.appendPlainText(log)
+
+    def log(self, message):
+        self.logging_texbox.appendPlainText(str(message))
+
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = MyWindow()
     MainWindow.show()
     sys.exit(app.exec())
+
+
+
+
--- serial_parse_test.py
+++ serial_parse_test.py
@@ -5,154 +5,181 @@ from serial import Serial
 
 import matplotlib.pyplot as plt
 
-BAUD_RATE = 230400
-STLINK_NAME = "STMicroelectronics STLink Virtual COM Port"
-NUM_CELLS = 70
 
-cell_data = {}
-# Cell Data Dictionary:
-"""
-cell_data = {
-    "cell_0": {
-        "voltage": 3.6,
-        "temp": 25,
+
+class SerialConnect(object):
+
+    BAUD_RATE = 230400
+    STLINK_NAME = "STMicroelectronics STLink Virtual COM Port"
+    NUM_CELLS = 70
+    def __init__(self):
+        self.ser = None
+        self.cell_data = {}
+        self.all_ports = []
+
+    # Cell Data Dictionary:
+    """
+    cell_data = {
+        "cell_0": {
+            "voltage": 3.6,
+            "temp": 25,
+        }
     }
-}
-"""
+    """
 
-def port_setup():
-    # Populate Cell Data dictionary:
-    for cell_num in range(NUM_CELLS):
-        cell_data.update({f"cell_{cell_num}": {"voltage": 0, "temp": 0}})
+    def port_setup(self):
+        # Populate Cell Data dictionary:
+        for cell_num in range(self.NUM_CELLS):
+            self.cell_data.update({f"cell_{cell_num}": {"voltage": 0, "temp": 0}})
 
-    # print(cell_data["cell_0"]["voltage"])
+        # print(cell_data["cell_0"]["voltage"])
 
-    # open the port
-    ports = serial.tools.list_ports.comports()
-    # list of all available ports
-    all_ports = []
-    message = ""
+        # open the port
+        ports = serial.tools.list_ports.comports()
+        # list of all available ports
 
-    for port, desc, hwid in sorted(ports):
-        # print("{}: {}".format(port, desc))    # Uncomment to see all ports found
-        all_ports.append("{}: {}".format(port, desc))
+        message = ""
 
-    ### connect to the port
-    serial_port = ""
-    port_found = False
+        for port, desc, hwid in sorted(ports):
+            # print("{}: {}".format(port, desc))    # Uncomment to see all ports found
+            self.all_ports.append("{}: {}".format(port, desc))
 
-    for p in all_ports:
-        curr = p.lower()
-        if STLINK_NAME.lower() in curr:
-            serial_port = p.split(':')[0]
-            port_found = True
-            message  = f"Found STLink on {serial_port}"
-            print(message)
+        ### connect to the port
+        serial_port = ""
+        port_found = False
 
-            # Setup Serial Connection
-            ser = Serial(serial_port, BAUD_RATE, stopbits=1, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS,
-                         timeout=0)
-            # Not so sure what this does
-            sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+        for p in self.all_ports:
+            curr = p.lower()
+            if self.STLINK_NAME.lower() in curr:
+                serial_port = p.split(':')[0]
+                port_found = True
+                message  = f"Found STLink on {serial_port}"
+                print(message)
 
-    if not port_found:
-        print("STLink not found! Ensure it is plugged in")
-        # insert a message to the log box: not connected
+                # Setup Serial Connection
+                ser = Serial(serial_port, self.BAUD_RATE, stopbits=1, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS,
+                             timeout=0)
+                # Not so sure what this does
+                sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
-        # not too sure what exit does here
-        # exit()
-        return "Failed"
+        if not port_found:
+            print("STLink not found! Ensure it is plugged in")
+            # insert a message to the log box: not connected
 
-    return message
+            # not too sure what exit does here
+            # exit()
+            return port_found
 
-
-####
+        return port_found
 
 
-def main():
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    for i in range(10):
-        # get battInfo commend
-        # input("Press Enter to get battInfo")
-        get_battInfo(ser)
-        print("Got battInfo", i+1)
-        for cell_name in cell_data.keys():
-            plt.scatter(i, cell_data[cell_name]["voltage"])
-        plt.pause(0.05)
-
-        print(cell_data)
-        time.sleep(2)
+    ####
 
 
-"""
------       Serial Parsing Code     -----
+    def execute(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        for i in range(10):
+            # get battInfo commend
+            # input("Press Enter to get battInfo")
+            self.get_battInfo(self.ser)
+            print("Got battInfo", i+1)
+            for cell_name in self.cell_data.keys():
+                plt.scatter(i, self.cell_data[cell_name]["voltage"])
+            plt.pause(0.05)
+
+            print(self.cell_data)
+            time.sleep(2)
 
 
-"""
+    """
+    -----       Serial Parsing Code     -----
+    
+    
+    """
 
 
-# Get battInfo and parse important information
-def get_battInfo(sio):
-    # sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-    sio.write("battInfo\n".encode())
-    # sio.flush()
-    time.sleep(0.2)  # Wait for response
-    raw_data = sio.read(sio.inWaiting())
-    data = raw_data.decode()
+    # Get battInfo and parse important information
+    def get_battInfo(self, sio):
+        # sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+        sio.write("battInfo\n".encode())
+        # sio.flush()
+        time.sleep(0.2)  # Wait for response
+        raw_data = sio.read(sio.inWaiting())
+        data = raw_data.decode()
 
-    if not data:
-        print("ERROR: NO DATA RECEIVED FROM BATTINFO CMD")
-        return "error"
+        if not data:
+            print("ERROR: NO DATA RECEIVED FROM BATTINFO CMD")
+            return "error"
 
-    else:
+        else:
 
-        # print(data)
+            # print(data)
 
-        ###     PARSE DATA FROM BATTINFO    ###
-        data = data.split("Index	Cell Voltage(V)	Temp Channel(degC)")[1].split("bmu >")[0]
-        # print(cell_data)
-        for line in data.split("\n"):
-            parsed = line.strip().split("\t")
-            if len(parsed) > 1:
-                try:
-                    # data Cell     Voltage     Temp
-                    cell_num = int(parsed[0])
-                    cell_voltage = parsed[1]
-                    cell_temp = parsed[2]
-
-
-                    # 70x3 array
-                    '''
-                    [[cell_num,cell_voltage, cell_temp], 
-                     [cell_num,cell_voltage, cell_temp],
-                     [cell_num,cell_voltage, cell_temp]]
-                    '''
+            ###     PARSE DATA FROM BATTINFO    ###
+            data = data.split("Index	Cell Voltage(V)	Temp Channel(degC)")[1].split("bmu >")[0]
+            # print(cell_data)
+            for line in data.split("\n"):
+                parsed = line.strip().split("\t")
+                if len(parsed) > 1:
+                    try:
+                        # data Cell     Voltage     Temp
+                        cell_num = int(parsed[0])
+                        cell_voltage = parsed[1]
+                        cell_temp = parsed[2]
 
 
-                    cell_key = f"cell_{cell_num}"
-
-                    # Populate dictionary entry
-                    if cell_key in cell_data.keys():
-                        cell_data[cell_key]["voltage"] = cell_voltage
-                        cell_data[cell_key]["temp"] = cell_temp
-
-                except Exception as e:      # TODO: Remove this once all parsing bugs are fixed
-                    print("Got Exception", e)
-                    print(parsed)
-                    print(cell_data)
-
-                # print(f"{cell_num} {cell_voltage} {cell_temp}")
+                        # 70x3 array
+                        '''
+                        [[cell_num,cell_voltage, cell_temp], 
+                         [cell_num,cell_voltage, cell_temp],
+                         [cell_num,cell_voltage, cell_temp]]
+                        '''
 
 
-def get_cell_data():
-    return cell_data
+                        cell_key = f"cell_{cell_num}"
 
-def get_port_name():
-    return ports
+                        # Populate dictionary entry
+                        if cell_key in self.cell_data.keys():
+                            self.cell_data[cell_key]["voltage"] = cell_voltage
+                            self.cell_data[cell_key]["temp"] = cell_temp
+
+                    except Exception as e:      # TODO: Remove this once all parsing bugs are fixed
+                        print("Got Exception", e)
+                        print(parsed)
+                        print(self.cell_data)
+
+                    # print(f"{cell_num} {cell_voltage} {cell_temp}")
+
+
+    # https://gist.github.com/nz-angel/31890d2c6cb1c9105e677cacc83a1ffd
+    def split_BatteryData(self, input_dict, chunk_size=14):
+        res = []
+        new_dict = {}
+        for k, v in input_dict.items():
+            if len(new_dict) < chunk_size:
+                new_dict[k] = v
+            else:
+                res.append(new_dict)
+                new_dict = {k: v}
+        res.append(new_dict)
+        return res
+
+
+    def get_cell_data(self):
+        # return a list of battery dictionary each entry has 14 cells
+        # return self.split_BatteryData(self.cell_data)
+        return self.cell_data
+
+    def get_port_name(self):
+        return self.all_ports
+
+    def get_cellNum(self):
+        return self.NUM_CELLS
 
 
 
-if __name__ == "__main__":
-    main()
+#
+# if __name__ == "__main__":
+#     main()
